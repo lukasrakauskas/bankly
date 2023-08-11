97e2d43f1d7172ab3b4c73d8aff160510009a5f1
Lukas Rakauskas <lukas.rakauskas@oxylabs.io>
2023-08-11 12:52:16 +0300

    Add data schemas

---
+++ src/schemas/account.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+import { balanceSchema } from "./balance";
+
+export const accountSchema = z
+  .object({
+    account_id: z.string(),
+    balance: balanceSchema,
+  })
+  .transform((it) => ({
+    accountId: it.account_id,
+    balance: it.balance,
+  }));
+
+export type Account = z.infer<typeof accountSchema>;
---
+++ src/schemas/transaction.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const transactionSchema = z.object({
+  id: z.string(),
+  date: z.string(),
+  description: z.string(),
+  category: z.string(),
+  amount: z
+    .object({
+      value: z.number(),
+      currency_iso: z.string(),
+    })
+    .transform((it) => ({ value: it.value, currencyIso: it.currency_iso })),
+});
+
+export type Transaction = z.infer<typeof transactionSchema>;
