fbdf9db4b15ba9b6fa96a223030591cb095b3454
Lukas Rakauskas <lukas.rakauskas@oxylabs.io>
2023-08-11 17:51:43 +0300

    Add header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+export const Header = () => (
+  <header>
+    <div className="content">
+      <a href="https://www.bankly.com/" target="_blank" rel="noreferrer">
+        <img src="/bankly.svg" className="logo" alt="Bankly logo" />
+      </a>
+    </div>
+  </header>
+);
